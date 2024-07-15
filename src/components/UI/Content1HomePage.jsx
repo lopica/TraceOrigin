295eb83d9f159349f5c7711a501ded84df3d1dc3
thanhnc94 <thanhnc942@gmail.com>
2024-07-15 18:40:23 +0700

    fix UI homepage

--- src/components/UI/Content1HomePage.jsx
+++ src/components/UI/Content1HomePage.jsx
@@ -4,16 +4,19 @@ import { FaArrowRight, FaSignInAlt, FaSearch } from "react-icons/fa";
 
 const Content1HomePage = () => {
   return (
-
     <div className="flex flex-col items-center justify-center p-10">
       {/* Tên nội dung */}
-      <h2 className="text-3xl font-bold mb-4"> Truy xuất nguồn gốc là gì?</h2>
+      <h2 className="text-3xl font-bold mb-4">
+        {" "}
+        Dịch vụ xác thực sản phẩm là gì?
+      </h2>
       <div className="max-w-4xl mx-auto p-6 text-center">
-            <strong>Truy xuất nguồn gốc sản phẩm </strong> là một giải pháp cho
-            người dùng truy xuất, tìm hiểu về thông tin nguồn gốc xuất xứ của
-            sản phẩm mà họ đã mua, truy ngược từ sản phẩm đang được bày bán trên
-            kệ hàng về nơi sản xuất ban đầu, rà soát từng công đoạn chế tác và phân phối.
-          </div>
+        Dịch vụ xác thực sản phẩm là một hệ thống cho phép người dùng xác minh
+        tính xác thực của một sản phẩm bằng cách truy xuất thông tin nguồn gốc
+        của sản phẩm, bao gồm nhà sản xuất, nơi sản xuất, chứng chỉ, lịch sử
+        giao dịch,.. Quá trình kiểm tra và xác minh rằng sản phẩm bạn mua là
+        hàng thật, không phải hàng giả, hàng nhái hoặc hàng kém chất lượng.
+      </div>
     </div>
   );
 };
