108b89ab328f235cc192dc030c9c28ccbcb04ff2
manhdt711 <dtm.it2002@gmail.com>
2024-08-11 14:29:12 +0700

    sua base 64 img

--- src/hooks/use-update-Images-FromApi.js
+++ src/hooks/use-update-Images-FromApi.js
@@ -31,13 +31,25 @@ export default function useUpdateImageFromApi() {
     let imagesShow = [];
 
     try {
+      // Convert listImages from URLs to base64
+      const base64Images = await Promise.all(listImages.map(urlToBase64));
+      
+      // Convert avatar from URL to base64
       const avatarBase64 = await urlToBase64(avatar);
+
+      // Process the list of images
       base64Images.forEach((base64) => {
         imageUrls.push(base64);
         imagesShow.push(`data:image/png;base64,${base64}`);
       });
+
+      // Dispatch actions to update the list of images
       dispatch(updateProductEditImages([...imagesShow]));
+
+      // Dispatch actions to update image data
       dispatch(updateProductEditImagesData([...imageUrls]));
+
+      // Update the avatar separately
       dispatch(updateProductEditAvatar(`data:image/png;base64,${avatarBase64}`));
       
     } catch (error) {
