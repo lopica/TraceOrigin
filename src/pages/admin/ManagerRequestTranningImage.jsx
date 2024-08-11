d1d623d26f0cec0dd842ad3b717fca7a67b53bc9
manhdt711 <dtm.it2002@gmail.com>
2024-08-11 17:12:10 +0700

    Sua manager Image

--- src/pages/admin/ManagerRequestTranningImage.jsx
+++ src/pages/admin/ManagerRequestTranningImage.jsx
@@ -9,6 +9,9 @@ import Pagination from "../../components/UI/Pagination";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import useToast from "../../hooks/use-toast";
+import { useForm } from "react-hook-form";
+import Button from "../../components/UI/Button";
+import handleKeyDown from "../../utils/handleKeyDown";
 
 const formatDate = (timestamp) => {
   if (!timestamp) return "N/A";
@@ -20,36 +23,54 @@ const ManagerRequestTranningImage = () => {
   const [slides, setSlides] = useState([]);
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [page, setPage] = useState(0);
-  const [filter, setFilter] = useState({
-    orderBy: "productId",
-    productName: "",
-    manufactorName: "",
-    isDesc: false,
-    page: 0,
-    size: 10,
-  });
   const [isLoading, setIsLoading] = useState(false);
   const navigate = useNavigate();
-  const [approvalImageRequest] = useApprovalImageRequestMutation();
   const { getToast } = useToast();
   const { isAuthenticated } = useSelector((state) => state.authSlice);
 
+  const { handleSubmit, register, reset } = useForm({
+    mode: "onTouched",
+    defaultValues: {
+      productName: "",
+      manufactorName: "",
+      orderBy: "productId",
+      desc: true,
+    },
+  });
+
+  const [searchParams, setSearchParams] = useState({
+    productName: "",
+    manufactorName: "",
+    orderBy: "productId",
+    desc: false,
+    page: page.toString(),
+    size: "10",
+  });
+
+  const { data, refetch, isError, isFetching } = useGetimageRequestQuery(searchParams, {
+    skip: !isAuthenticated,
+  });
+
+  const [approvalImageRequest] = useApprovalImageRequestMutation();
+
   useEffect(() => {
-    if (!isAuthenticated) {
+    if (isAuthenticated) {
+      refetch();
+    }
+  }, [isAuthenticated, refetch]);
+
+  useEffect(() => {
+    if (isError?.status === 401 && !isFetching) {
+      navigate("/portal/login");
+    }
+  }, [isError, navigate]);
+
+  useEffect(() => {
+    if (!isFetching && !isAuthenticated) {
       getToast("Phiên đăng nhập đã hết hạn");
       navigate("/portal/login");
     }
-  }, [isAuthenticated]);
-
-  const { data, refetch } = useGetimageRequestQuery(filter, {
-    skip: false,
-    refetchOnFocus: false,
-  });
-
-  // useEffect(() => {
-  //   setIsLoading(true);
-  //   refetch().finally(() => setIsLoading(false));
-  // }, [refetch]);
+  }, [isFetching, isAuthenticated, getToast, navigate]);
 
   const handleViewFiles = (files) => {
     const images = files.map((file) => file);
@@ -119,166 +140,218 @@ const ManagerRequestTranningImage = () => {
     }
   };
 
-  const handleFilterChange = (e) => {
-    const { name, value, type, checked } = e.target;
-    setFilter({
-      ...filter,
-      [name]: type === "checkbox" ? checked : value,
-    });
-  };
-
-  const handleBlur = () => {
-    // // Tự động tìm kiếm khi trường input bị blur
-    // setIsLoading(true);
-    // refetch().finally(() => setIsLoading(false));
-  };
-
   const handlePageChange = (newPage) => {
     setPage(newPage);
-    setFilter({
-      ...filter,
-      page: newPage,
+    setSearchParams((prevParams) => ({
+      ...prevParams,
+      page: newPage.toString(),
+    }));
+    refetch();
+  };
+
+  const searchHandler = (data) => {
+    setSearchParams({
+      ...searchParams,
+      productName: data.productName,
+      manufactorName: data.manufactorName,
+      orderBy: data.orderBy,
+      desc: data.desc,
+      page: 0
     });
+  };
+
+  const resetHandler = () => {
+    reset({
+      productName: "",
+      manufactorName: "",
+      orderBy: "productId",
+      desc: false,
+    });
+    setSearchParams({
+      ...searchParams,
+      productName: "",
+      manufactorName: "",
+      orderBy: "productId",
+      desc: false,
+      page: 0
+    });
+    refetch();
+  };
+
+  const renderTableBody = () => {
+    if (isFetching) {
+      return (
+        <tbody>
+          <tr>
+            <td colSpan="4" className="text-center">
+              <span className="loading loading-spinner loading-lg"></span>
+            </td>
+          </tr>
+        </tbody>
+      );
+    }
+
+    if (isError) {
+      return (
+        <tbody>
+          <tr>
+            <td colSpan="4" className="text-center text-red-500">
+              Error fetching data.
+            </td>
+          </tr>
+        </tbody>
+      );
+    }
+
+    const requests = data?.content || [];
+    return (
+      <tbody>
+        {requests.map((request, index) => (
+          <tr key={index} className="hover">
+            <td>{index + 1 + page * 10}</td>
+            <td>{request.productName}</td>
+            <td>{request.manufactorName}</td>
+            <td>{formatDate(request.requestDate)}</td>
+            <td className="flex justify-center space-x-2">
+              <button
+                onClick={() => handleViewFiles(request.filePath)}
+                className="btn btn-primary text-white btn-xs"
+              >
+                <FaEye />
+              </button>
+              <button
+                onClick={() => handleDownloadFiles(request.filePath, request.productName, request.manufactorName)}
+                className="btn btn-success text-white btn-xs"
+              >
+                <FaDownload />
+              </button>
+              <button
+                onClick={() => handleApproval(request.productId)}
+                className="btn btn-info text-white btn-xs"
+                disabled={isLoading}
+              >
+                {isLoading ? <span className="loading loading-spinner loading-xs"></span> : "Phê Duyệt"}
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    );
   };
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <div className="bg-gray-100 p-6 rounded-lg shadow-md">
-        <h2 className="text-xl font-semibold mb-4">Tìm kiếm</h2>
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-          <div>
-            <label htmlFor="productName" className="block font-medium mb-1">Tên sản phẩm</label>
-            <input
-              type="text"
-              id="productName"
-              name="productName"
-              value={filter.productName}
-              onChange={handleFilterChange}
-              onBlur={handleBlur}
-              className="input input-bordered w-full"
-              placeholder="Tìm theo tên sản phẩm"
-            />
-          </div>
-          <div>
-            <label htmlFor="manufactorName" className="block font-medium mb-1">Nhà sản xuất</label>
-            <input
-              type="text"
-              id="manufactorName"
-              name="manufactorName"
-              value={filter.manufactorName}
-              onChange={handleFilterChange}
-              onBlur={handleBlur}
-              className="input input-bordered w-full"
-              placeholder="Tìm theo nhà sản xuất"
-            />
-          </div>
-          <div>
-            <label htmlFor="orderBy" className="block font-medium mb-1">Sắp xếp theo</label>
-            <select
-              id="orderBy"
-              name="orderBy"
-              value={filter.orderBy}
-              onChange={handleFilterChange}
-              onBlur={handleBlur}
-              className="select select-bordered w-full"
-            >
-              <option value="productId">Mã sản phẩm</option>
-              <option value="productName">Tên sản phẩm</option>
-              <option value="requestScanDate">Ngày yêu cầu</option>
-            </select>
-          </div>
-        </div>
-        <div className="flex items-center mt-4">
+      <form
+        className="flex flex-col md:flex-row md:flex-wrap items-end gap-4 mx-auto"
+        onKeyDown={handleKeyDown}
+        onSubmit={handleSubmit(searchHandler)}
+      >
+        <div className="w-full md:w-1/3 lg:w-1/4">
+          <label htmlFor="productName" className="block text-sm font-medium text-gray-700">
+            Tên sản phẩm
+          </label>
           <input
-            type="checkbox"
-            id="isDesc"
-            name="isDesc"
-            checked={filter.isDesc}
-            onChange={handleFilterChange}
-            onBlur={handleBlur}
-            className="checkbox"
+            id="productName"
+            {...register("productName")}
+            type="search"
+            className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            placeholder="Tìm theo tên sản phẩm"
           />
-          <label htmlFor="isDesc" className="font-medium ml-2">Sắp xếp giảm dần</label>
         </div>
-      </div>
-      <table className="min-w-full bg-white shadow-md rounded-lg overflow-hidden mt-4">
-        <thead className="bg-gray-200">
-          <tr>
-            <th className="py-2 px-4 font-semibold text-left">Tên Sản Phẩm</th>
-            <th className="py-2 px-4 font-semibold text-left">Nhà Sản Xuất</th>
-            <th className="py-2 px-4 font-semibold text-left">Ngày Yêu Cầu</th>
-            <th className="py-2 px-4 font-semibold">Thao Tác</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data?.content?.map((request, index) => (
-            <tr key={index} className="text-center border-b hover:bg-gray-100">
-              <td className="py-2 px-4 text-left">{request.productName}</td>
-              <td className="py-2 px-4 text-left">{request.manufactorName}</td>
-              <td className="py-2 px-4">{formatDate(request.requestDate)}</td>
-              <td className="py-2 px-4 flex justify-center space-x-2">
-                <button
-                  onClick={() => handleViewFiles(request.filePath)}
-                  className="btn btn-primary text-white btn-xs"
-                >
-                  <FaEye />
-                </button>
-                <button
-                  onClick={() => handleDownloadFiles(request.filePath, request.productName, request.manufactorName)}
-                  className="btn btn-success text-white btn-xs"
-                >
-                  <FaDownload />
-                </button>
-                <button
-                  onClick={() => handleApproval(request.productId)}
-                  className="btn btn-info text-white btn-xs"
-                  disabled={isLoading}
-                >
-                  {isLoading ? <span className="loading loading-spinner loading-xs"></span> : "Phê Duyệt"}
-                </button>
-              </td>
+        <div className="w-full md:w-1/3 lg:w-1/4">
+          <label htmlFor="manufactorName" className="block text-sm font-medium text-gray-700">
+            Nhà sản xuất
+          </label>
+          <input
+            id="manufactorName"
+            {...register("manufactorName")}
+            type="search"
+            className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            placeholder="Tìm theo nhà sản xuất"
+          />
+        </div>
+        <div className="w-full md:w-1/3 lg:w-1/4">
+          <label htmlFor="orderBy" className="block text-sm font-medium text-gray-700">
+            Sắp xếp theo
+          </label>
+          <select
+            id="orderBy"
+            {...register("orderBy")}
+            className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          >
+            <option value="productId">Mã sản phẩm</option>
+            <option value="productName">Tên sản phẩm</option>
+            <option value="manufactorName">Tên nhà sản xuất</option>
+          </select>
+        </div>
+        <div className="w-full md:w-1/3 lg:w-1/4">
+          <label htmlFor="desc" className="block text-sm font-medium text-gray-700">
+            Thứ tự
+          </label>
+          <select
+            id="desc"
+            {...register("desc")}
+            className="mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+          >
+            <option value={false}>Tăng dần</option>
+            <option value={true}>Giảm dần</option>
+          </select>
+        </div>
+        <div className="flex items-end gap-2">
+          <Button  primary rounded  type="submit">
+            Tìm kiếm
+          </Button>
+          <Button type="button" onClick={resetHandler} secondary  rounded>
+            Đặt lại
+          </Button>
+        </div>
+      </form>
+
+      <div className="overflow-x-auto mt-8">
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Tên sản phẩm</th>
+              <th>Nhà sản xuất</th>
+              <th>Ngày yêu cầu</th>
+              <th className="text-center">Hành động</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-      <div className="mt-4 flex justify-between">
-        <button
-          onClick={handleDownloadAllFiles}
-          className="btn btn-warning"
-          disabled={isLoading}
-        >
-          {isLoading ? <span className="loading loading-spinner loading-xs"></span> : "Tải Xuống Tất Cả"}
-        </button>
-        <Pagination
-          currentPage={page}
-          totalPages={data?.totalPages || 0}
-          onPageChange={handlePageChange}
-        />
+          </thead>
+          {renderTableBody()}
+        </table>
       </div>
+
+      <div className="flex justify-between mt-4">
+      <Button onClick={handleDownloadAllFiles} className="btn btn-success text-white">
+          Tải xuống tất cả
+        </Button>
+      <Pagination
+        active={page}
+        totalPage={data?.totalPages || 0}
+        onPageChange={handlePageChange}
+      />
+      </div>
+
       <Modal
         isOpen={modalIsOpen}
         onRequestClose={() => setModalIsOpen(false)}
-        contentLabel="Image Viewer"
-        style={{
-          content: {
-            top: '50%',
-            left: '50%',
-            right: 'auto',
-            bottom: 'auto',
-            marginRight: '-50%',
-            transform: 'translate(-50%, -50%)',
-            width: '80%',
-            maxHeight: '80%'
-          },
-        }}
+        contentLabel="Image Carousel"
+        ariaHideApp={false}
+        className="fixed inset-0 flex items-center justify-center z-50"
+        overlayClassName="fixed inset-0 bg-black bg-opacity-50"
       >
-        <button
-          onClick={() => setModalIsOpen(false)}
-          className="btn btn-danger absolute top-2 right-2"
-        >
-          x
-        </button>
-        <Carousel slides={slides} />
+        <div className="bg-white rounded-lg p-6">
+          <Carousel slides={slides} />
+          <div className="flex justify-end mt-4">
+            <Button
+              onClick={() => setModalIsOpen(false)}
+              danger
+            >
+              Đóng
+            </Button>
+          </div>
+        </div>
       </Modal>
     </div>
   );
