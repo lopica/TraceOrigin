2b37c1fa703162f655cf470f2590d75fa91d01cf
thanhnc94 <thanhnc942@gmail.com>
2024-08-15 09:44:35 +0700

    fix ui

--- src/pages/manufacturer/ManuReportManager.jsx
+++ src/pages/manufacturer/ManuReportManager.jsx
@@ -1,8 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
-import Modal from 'react-modal';
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import Modal from "react-modal";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { useGetListReportsQuery, useReplyReportMutation } from '../../store/index';
+import {
+  useGetListReportsQuery,
+  useReplyReportMutation,
+} from "../../store/index";
 import Button from "../../components/UI/Button";
 import {
   FaUser,
@@ -18,43 +21,67 @@ import {
   FaQuestionCircle,
   FaArrowDown,
   FaArrowUp,
-  FaArrowRight
-} from 'react-icons/fa';
+  FaArrowRight,
+  FaExclamationTriangle,
+} from "react-icons/fa";
 import Pagination from "../../components/UI/Pagination";
-import InputTextModal from '../../components/UI/InputTextModal';
+import InputTextModal from "../../components/UI/InputTextModal";
 import { useSelector } from "react-redux";
 import useToast from "../../hooks/use-toast";
 
+
 const statusOptions = [
-  { value: 0, label: 'Mở', icon: <FaHourglassHalf className="text-yellow-500" /> },
-  { value: 1, label: 'Đã giải quyết', icon: <FaCheckCircle className="text-green-500" /> },
+  {
+    value: 0,
+    label: "Mở",
+    icon: <FaHourglassHalf className="text-yellow-500" />,
+  },
+  {
+    value: 1,
+    label: "Đã giải quyết",
+    icon: <FaCheckCircle className="text-green-500" />,
+  },
 ];
 
 const priorityOptions = [
-  { value: 0, label: 'Low', icon: <FaArrowDown className="text-green-500" /> },
-  { value: 1, label: 'Medium', icon: <FaArrowRight className="text-yellow-500" /> },
-  { value: 2, label: 'High', icon: <FaArrowUp className="text-orange-500" /> },
-  { value: 3, label: 'Very High', icon: <FaArrowUp className="text-red-500" /> }
+  { value: 0, label: "Low", icon: <FaArrowDown className="text-green-500" /> },
+  {
+    value: 1,
+    label: "Medium",
+    icon: <FaArrowRight className="text-yellow-500" />,
+  },
+  { value: 2, label: "High", icon: <FaArrowUp className="text-orange-500" /> },
+  {
+    value: 3,
+    label: "Very High",
+    icon: <FaArrowUp className="text-red-500" />,
+  },
 ];
 
 const typeOptions = [
-  { value: 1, icon: <FaExclamationCircle className="text-red-500" />, label: 'Lỗi' },
-  { value: 2, icon: <FaQuestionCircle className="text-blue-500" />, label: 'Hỏi đáp' }
+  {
+    value: 1,
+    icon: <FaExclamationCircle className="text-red-500" />,
+    label: "Lỗi",
+  },
+  {
+    value: 2,
+    icon: <FaQuestionCircle className="text-blue-500" />,
+    label: "Hỏi đáp",
+  },
 ];
 
-Modal.setAppElement('#root');
+Modal.setAppElement("#root");
 
 function ManuReportManager() {
   const { id } = useParams();
   const navigate = useNavigate();
   const [page, setPage] = useState(0);
   const [size, setSize] = useState(6);
-  const userIdList = useSelector(state => state.userSlice.userId);
-  const {
-    list: products
-  } = useSelector((state) => state.productSlice);
+  const userIdList = useSelector((state) => state.userSlice.userId);
+  const { list: products } = useSelector((state) => state.productSlice);
 
-  const { data, error, isLoading, refetch} = useGetListReportsQuery({
+  const { data, error, isLoading, refetch } = useGetListReportsQuery({
     code: "",
     title: "",
     reportTo: userIdList,
@@ -67,7 +94,7 @@ function ManuReportManager() {
     page,
     size,
     emailReport: "",
-    productId: ""
+    productId: "",
   });
   const { getToast } = useToast();
   const [replyReport] = useReplyReportMutation();
@@ -77,10 +104,10 @@ function ManuReportManager() {
   const [showImageReports, setShowImageReports] = useState(true);
   const [showInfo, setShowInfo] = useState(true);
   const [modalIsOpen, setModalIsOpen] = useState(false);
-  const [currentImage, setCurrentImage] = useState('');
+  const [currentImage, setCurrentImage] = useState("");
   const [status, setStatus] = useState(-1);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [textAreaValue, setTextAreaValue] = useState('');
+  const [textAreaValue, setTextAreaValue] = useState("");
   const { isAuthenticated } = useSelector((state) => state.authSlice);
 
   useEffect(() => {
@@ -96,7 +123,7 @@ function ManuReportManager() {
 
   useEffect(() => {
     if (data) {
-      const issue = data.content.find(issue => issue.id === parseInt(id));
+      const issue = data.content.find((issue) => issue.id === parseInt(id));
       setSelectedIssue(issue || data.content[0]);
       setStatus(data.content.status ?? -1);
     }
@@ -114,7 +141,10 @@ function ManuReportManager() {
         setStatus(1);
         setIsModalOpen(true);
       }
-      setSelectedIssue({ ...selectedIssue, status: parseInt(event.target.value) });
+      setSelectedIssue({
+        ...selectedIssue,
+        status: parseInt(event.target.value),
+      });
     }
   };
 
@@ -140,7 +170,7 @@ function ManuReportManager() {
   };
 
   const closeModal = () => {
-    if(status === 1) {
+    if (status === 1) {
       setStatus(0);
     }
     setModalIsOpen(false);
@@ -151,12 +181,14 @@ function ManuReportManager() {
   };
 
   const getTypeIcon = (type) => {
-    const typeOption = typeOptions.find(option => option.value === type);
+    const typeOption = typeOptions.find((option) => option.value === type);
     return typeOption ? typeOption.icon : null;
   };
 
   const getPriorityIcon = (priority) => {
-    const priorityOption = priorityOptions.find(option => option.value === priority);
+    const priorityOption = priorityOptions.find(
+      (option) => option.value === priority
+    );
     return priorityOption ? priorityOption.icon : null;
   };
 
@@ -164,7 +196,8 @@ function ManuReportManager() {
     return (
       <div className="flex h-screen font-sans bg-gray-100">
         <div className="w-1/4 bg-white border-r border-gray-300 overflow-y-auto">
-            <span className="loading loading-spinner loading-lg text-center"></span>;
+          <span className="loading loading-spinner loading-lg text-center"></span>
+          ;
         </div>
       </div>
     );
@@ -174,9 +207,16 @@ function ManuReportManager() {
     return <div>Error: {error.message}</div>;
   }
 
-  if(data.content.length < 1)
-  {
-    return <span className='p-4'>Bạn chưa có phản hồi nào về các sản phẩm</span>;
+  if (data.content.length < 1) {
+    return (
+      <div className="flex flex-col items-center justify-center h-[92vh]">
+      <FaExclamationTriangle className="text-yellow-500 text-4xl mb-4" />
+        <h2 className="text-xl font-bold mb-4">Không có phản hồi</h2>
+        <p className="text-gray-600">
+        Bạn chưa có phản hồi nào về các sản phẩm mà bạn đã đăng ký từ người dùng.
+        </p>
+      </div>
+    );
   }
 
   const closeConfirmModal = () => {
@@ -195,10 +235,10 @@ function ManuReportManager() {
         responseDetail: textAreaValue,
       }).unwrap();
       refetch();
-      console.log('Reply sent successfully');
+      console.log("Reply sent successfully");
       closeConfirmModal();
     } catch (error) {
-      console.error('Failed to send reply:', error);
+      console.error("Failed to send reply:", error);
     }
   };
 
@@ -246,12 +286,13 @@ function ManuReportManager() {
         </form> */}
         <div className="p-0">
           <ul className="list-none p-0 m-0">
-            
             {data.content.map((issue) => (
               <li
                 key={issue.id}
                 className={`p-3 cursor-pointer border-b border-gray-200 ${
-                  selectedIssue && selectedIssue.id === issue.id ? 'bg-gray-300 text-gray-800' : 'hover:bg-gray-200'
+                  selectedIssue && selectedIssue.id === issue.id
+                    ? "bg-gray-300 text-gray-800"
+                    : "hover:bg-gray-200"
                 }`}
                 onClick={() => handleIssueClick(issue)}
               >
@@ -268,10 +309,13 @@ function ManuReportManager() {
               </li>
             ))}
           </ul>
-          <div className='p-2 text-right'>
-            { data.totalPages > 1 &&
-            (
-              <Pagination active={page} totalPages={data.totalPages || 0} onPageChange={handlePageChange} />
+          <div className="p-2 text-right">
+            {data.totalPages > 1 && (
+              <Pagination
+                active={page}
+                totalPages={data.totalPages || 0}
+                onPageChange={handlePageChange}
+              />
             )}
           </div>
         </div>
@@ -285,21 +329,35 @@ function ManuReportManager() {
               </p>
             </div>
             <div className="flex items-center mb-4">
-              <div className="text-3xl flex items-center justify-center">{getTypeIcon(selectedIssue.type)}</div>
-              <h2 className="text-2xl font-bold ml-3 flex items-center">{selectedIssue.title}</h2>
+              <div className="text-3xl flex items-center justify-center">
+                {getTypeIcon(selectedIssue.type)}
+              </div>
+              <h2 className="text-2xl font-bold ml-3 flex items-center">
+                {selectedIssue.title}
+              </h2>
             </div>
             <div className="grid grid-cols-2 gap-4">
               <div className="font-medium">
-                <span className="font-bold">Mã sản phẩm:</span> {selectedIssue.itemId}
+                <span className="font-bold">Mã sản phẩm:</span>{" "}
+                {selectedIssue.itemId}
               </div>
               <div className="font-medium text-right">
-                <span className="font-bold">Tên sản phẩm:</span> {selectedIssue.productName}
+                <span className="font-bold">Tên sản phẩm:</span>{" "}
+                {selectedIssue.productName}
               </div>
               <div className="col-span-2 flex justify-between items-center">
                 <div className="font-medium flex items-center">
                   <span className="font-bold">Mức độ ưu tiên:</span>
-                  <span className="ml-2">{priorityOptions.find(option => option.value === selectedIssue.priority).label}</span>
-                  <span className="ml-2">{getPriorityIcon(selectedIssue.priority)}</span>
+                  <span className="ml-2">
+                    {
+                      priorityOptions.find(
+                        (option) => option.value === selectedIssue.priority
+                      ).label
+                    }
+                  </span>
+                  <span className="ml-2">
+                    {getPriorityIcon(selectedIssue.priority)}
+                  </span>
                 </div>
                 <div className="font-medium">
                   <span className="font-bold">Trạng thái:</span>
@@ -322,28 +380,42 @@ function ManuReportManager() {
         {selectedIssue && (
           <>
             <div className="bg-white p-6 rounded-lg shadow-md mt-4">
-              <h3 className="text-xl font-semibold mb-2 flex justify-between items-center cursor-pointer" onClick={toggleCauseDetail}>
-                Nguyên nhân <span>{showCauseDetail ? <FaChevronUp /> : <FaChevronDown />}</span>
+              <h3
+                className="text-xl font-semibold mb-2 flex justify-between items-center cursor-pointer"
+                onClick={toggleCauseDetail}
+              >
+                Nguyên nhân{" "}
+                <span>
+                  {showCauseDetail ? <FaChevronUp /> : <FaChevronDown />}
+                </span>
               </h3>
               {showCauseDetail && (
-                <p className="font-medium">
-                  {selectedIssue.causeDetail}
-                </p>
+                <p className="font-medium">{selectedIssue.causeDetail}</p>
               )}
             </div>
             <div className="bg-white p-6 rounded-lg shadow-md mt-4">
-              <h3 className="text-xl font-semibold mb-2 flex justify-between items-center cursor-pointer" onClick={toggleResponseDetail}>
-                Phản hồi <span>{showResponseDetail ? <FaChevronUp /> : <FaChevronDown />}</span>
+              <h3
+                className="text-xl font-semibold mb-2 flex justify-between items-center cursor-pointer"
+                onClick={toggleResponseDetail}
+              >
+                Phản hồi{" "}
+                <span>
+                  {showResponseDetail ? <FaChevronUp /> : <FaChevronDown />}
+                </span>
               </h3>
               {showResponseDetail && (
-                <p className="font-medium">
-                  {selectedIssue.responseDetail}
-                </p>
+                <p className="font-medium">{selectedIssue.responseDetail}</p>
               )}
             </div>
             <div className="bg-white p-6 rounded-lg shadow-md mt-4">
-              <h3 className="text-xl font-semibold mb-2 flex justify-between items-center cursor-pointer" onClick={toggleImageReports}>
-                Báo cáo hình ảnh <span>{showImageReports ? <FaChevronUp /> : <FaChevronDown />}</span>
+              <h3
+                className="text-xl font-semibold mb-2 flex justify-between items-center cursor-pointer"
+                onClick={toggleImageReports}
+              >
+                Báo cáo hình ảnh{" "}
+                <span>
+                  {showImageReports ? <FaChevronUp /> : <FaChevronDown />}
+                </span>
               </h3>
               {showImageReports && (
                 <div className="grid grid-cols-6 gap-1">
@@ -365,36 +437,44 @@ function ManuReportManager() {
       <div className="w-1/4 pt-4 pr-4 bg-gray-100 overflow-y-auto">
         {selectedIssue && (
           <div className="bg-white p-6 rounded-lg shadow-md">
-            <h3 className="text-xl font-semibold mb-2 flex justify-between items-center cursor-pointer" onClick={toggleInfo}>
-              Thông tin <span>{showInfo ? <FaChevronUp /> : <FaChevronDown />}</span>
+            <h3
+              className="text-xl font-semibold mb-2 flex justify-between items-center cursor-pointer"
+              onClick={toggleInfo}
+            >
+              Thông tin{" "}
+              <span>{showInfo ? <FaChevronUp /> : <FaChevronDown />}</span>
             </h3>
             {showInfo && (
               <>
                 <p className="font-medium flex items-center">
                   <FaUser className="mr-2 text-gray-500" />
                   <div>
-                    <span className="font-bold">Người tạo:</span><br />
+                    <span className="font-bold">Người tạo:</span>
+                    <br />
                     {selectedIssue.createBy}
                   </div>
                 </p>
                 <p className="font-medium flex items-center">
                   <FaUser className="mr-2 text-gray-500" />
                   <div>
-                    <span className="font-bold">Báo cáo cho:</span><br />
+                    <span className="font-bold">Báo cáo cho:</span>
+                    <br />
                     {selectedIssue.reportTo.name}
                   </div>
                 </p>
                 <p className="font-medium flex items-center">
                   <FaCalendarAlt className="mr-2 text-gray-500" />
                   <div>
-                    <span className="font-bold">Ngày tạo:</span><br />
+                    <span className="font-bold">Ngày tạo:</span>
+                    <br />
                     {new Date(selectedIssue.createOn).toLocaleDateString()}
                   </div>
                 </p>
                 <p className="font-medium flex items-center">
                   <FaCalendarAlt className="mr-2 text-gray-500" />
                   <div>
-                    <span className="font-bold">Ngày cập nhật:</span><br />
+                    <span className="font-bold">Ngày cập nhật:</span>
+                    <br />
                     {new Date(selectedIssue.updateOn).toLocaleDateString()}
                   </div>
                 </p>
@@ -409,32 +489,36 @@ function ManuReportManager() {
         onRequestClose={closeModal}
         style={{
           content: {
-            position: 'absolute',
-            top: '50%',
-            left: '50%',
-            right: 'auto',
-            bottom: 'auto',
-            transform: 'translate(-50%, -50%)',
-            background: 'white',
-            padding: '20px',
-            borderRadius: '10px',
-            outline: 'none',
-            boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)'
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            right: "auto",
+            bottom: "auto",
+            transform: "translate(-50%, -50%)",
+            background: "white",
+            padding: "20px",
+            borderRadius: "10px",
+            outline: "none",
+            boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
           },
           overlay: {
-            position: 'fixed',
+            position: "fixed",
             top: 0,
             left: 0,
             right: 0,
             bottom: 0,
-            backgroundColor: 'rgba(0, 0, 0, 0.75)',
-            zIndex: 1000
-          }
+            backgroundColor: "rgba(0, 0, 0, 0.75)",
+            zIndex: 1000,
+          },
         }}
         shouldCloseOnOverlayClick={true}
       >
         <div className="flex justify-center items-center h-full">
-          <img src={currentImage} alt="Current Report" className="max-w-full max-h-full" />
+          <img
+            src={currentImage}
+            alt="Current Report"
+            className="max-w-full max-h-full"
+          />
         </div>
       </Modal>
     </div>
