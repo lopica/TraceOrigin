1ce2f671383fb1752b45411ae44a00fae2ffa95e
lopica <ducanhedison@gmail.com>
2024-08-25 16:51:42 +0700

    check email

--- src/store/apis/utilApi.js
+++ src/store/apis/utilApi.js
@@ -6,7 +6,6 @@ const utilApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: `${CONSTANTS.domain}`,
     fetchFn: async (input, init, ...args) => {
-
       const url = typeof input === "string" ? input : input.url;
       if (url.includes("/eventtype/getListEventType")) {
         init = {
@@ -24,11 +23,26 @@ const utilApi = createApi({
         query: () => "/transport/getAllTransport",
       }),
       getAllEventType: builder.query({
-        query: () => '/eventtype/getListEventType' 
-      })
+        query: () => "/eventtype/getListEventType",
+      }),
+      checkEmail: builder.mutation({
+        query: (email) => ({
+          url: `https://melink.vn/checkmail/checkemail.php`,
+          method: "POST",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+          body: new URLSearchParams({ email }).toString(),
+          responseHandler: res => res.text()
+        }),
+      }),
     };
   },
 });
 
 export { utilApi };
-export const { useGetAllTransportsQuery, useGetAllEventTypeQuery } = utilApi;
+export const {
+  useGetAllTransportsQuery,
+  useGetAllEventTypeQuery,
+  useCheckEmailMutation,
+} = utilApi;
